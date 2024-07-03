3bb333cfd3fd47d8d1cc81ed5b225a988262642b
brahyerr <head@phams.page>
2024-07-03 17:48:50 -0400

    Fix .gitignore patterns, add fast-scroll and ready-player

--- config/media-player.el
+++ config/media-player.el
@@ -2,4 +2,9 @@
 (use-package emms)
 (setq emms-player-list '(mpv))
 
+;; ready-player requires ffprobe from ffmpeg for metadata
+(load-file (expand-file-name "config/ready-player.el" user-emacs-directory))
+(require 'ready-player)
+(ready-player-add-to-auto-mode-alist)
+
 (provide 'media-player)
