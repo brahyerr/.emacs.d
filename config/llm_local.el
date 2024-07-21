a4fad0b6a4e772c14013481f4df9f7847d3ea355
brahyerr <head@phams.page>
2024-07-21 17:23:32 -0400

    Change llm selection based on host

--- config/llm_local.el
+++ config/llm_local.el
@@ -9,13 +9,18 @@
 	  (make-llm-ollama
 	   ;; this model should be pulled to use it
 	   ;; value should be the same as you print in terminal during pull
-	   :chat-model "llama3:8b-instruct-q8_0"
+	   :chat-model (pcase (system-name)
+			 ("nix-STATION" "llama3:8b-instruct-q8_0")
+			 ("nixpad"      "codegemma:instruct"))
 	   :embedding-model "nomic-embed-text"
 	   :default-chat-non-standard-params '(("num_ctx" . 8192))))
   ;; Naming new sessions with llm
   (setopt ellama-naming-provider
 	  (make-llm-ollama
-	   :chat-model "llama3:8b-instruct-q8_0"
+	   :chat-model (pcase (system-name)
+			 ("nix-STATION" "llama3:8b-instruct-q8_0")
+			 ("nixpad"      "codegemma:instruct"))
+
 	   :embedding-model "nomic-embed-text"
 	   :default-chat-non-standard-params '(("stop" . ("\n")))))
   (setopt ellama-naming-scheme 'ellama-generate-name-by-llm))
